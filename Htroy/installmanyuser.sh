d650f929abff751e0605c95fd4991a7372c31d7f
Htroy <htroy@htroy.com>
2019-08-16 01:47:35 +0800

    Update installmanyuser.sh

--- Htroy/installmanyuser.sh
+++ Htroy/installmanyuser.sh
@@ -27,11 +27,14 @@ prepare() {
 
 install_libsodium() {
 	echo -e "Installing libsodium"
-	wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
-	tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
-	./configure && make -j2 && make install
-	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-	ldconfig
+	#wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+	#tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+	#./configure && make -j2 && make install
+	#echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+	#ldconfig
+	yum install epel-release -y
+	yum makecache fast
+	yum install libsodium -y
 }
 
 get_server() {
