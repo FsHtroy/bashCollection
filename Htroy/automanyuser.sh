ac52585b33640816fe937f604f6185392e5da6b5
Htroy <htroy@htroy.com>
2019-09-22 13:05:31 +0800

    Update automanyuser.sh

--- Htroy/automanyuser.sh
+++ Htroy/automanyuser.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+yum_update() {
+  yum update -y
+}
+
+yum_install() {
+  yum install wget vim screen git epel-release net-tools curl mtr traceroute telnet -y
+  yum makecache fast
+  yum install htop bash-completion libsodium -y
+  yum groupinstall "Development tools" -y
+}
+
+pip_install() {
+  curl -fsSL https://bootstrap.pypa.io/get-pip.py|python
+}
+
+systemctl() {
+  systemctl disable firewalld
+  systemctl disable iptables
+  systemctl stop firewalld
+  systemctl stop iptables
+}
+
+write_rclocal() {
+  chmod +x /etc/rc.d/rc.local
+  sed -i '$i\chattr -i /serverspeeder/etc/apx* && /serverspeeder/bin/serverSpeeder.sh uninstall -f
+ && wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh && bash serverspeeder.sh' /etc/rc.local
+}
+
+change_kernel() {
+  rpm -ivh http://soft.91yun.pw/ISO/Linux/CentOS/kernel/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
+}
+
+yum_update
+yum_install
+pip_install
+systemctl
+write_rclocal
+change_kernel
