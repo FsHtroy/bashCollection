849d5ebea91ab083ef1d61fdd13f91e4e2b536e9
Htroy <htroy@htroy.com>
2021-04-24 22:56:58 +0800

    Update installsupervisor.sh

--- Htroy/installsupervisor.sh
+++ Htroy/installsupervisor.sh
@@ -22,7 +22,7 @@ check_root() {
 
 install_pip() {
 	if [[ "${pip}" != "installed" ]]; then
-		curl https://bootstrap.pypa.io/2.7/get-pip.py --output get-pip.py
+		curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 		python get-pip.py
 	fi
 }
